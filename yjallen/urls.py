ba8dbc64784dd986d518bd6af41c53ee910c2a5f
Sara Palmer <sepalme@emory.edu>
2014-09-12 14:45:34 -0400

    update

--- yjallen/urls.py
+++ yjallen/urls.py
@@ -1,27 +1,23 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from django.views.generic.simple import redirect_to
-from django.contrib.sitemaps import views as sitemaps_views
-from django.contrib.sitemaps import Sitemap
+#from django.contrib.sitemaps import views as sitemaps_views
+#from django.contrib.sitemaps import Sitemap
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 from yjallen_app.models import LetterTitle
-from yjallen_app.sitemaps import LetterSitemap
+#from yjallen_app.sitemaps import LetterSitemap
 from yjallen_app.views import index, overview, letter_display, searchbox, letter_xml
 
 # info_dict = {
 #   'queryset': LetterTitle.objects.all(),
 # }
 
-sitemaps = {
-    'letters': LetterSitemap,
-}
+# sitemaps = {
+#     'letters': LetterSitemap,
+# }
 
 urlpatterns = patterns('yjallen_app.views',
     url(r'^$', 'index', name='index'),
@@ -29,8 +25,7 @@ urlpatterns = patterns('yjallen_app.views',
     url(r'^(?P<doc_id>[^/]+)$', 'letter_display', name="letter_display"),
     url(r'^(?P<doc_id>[^/]+)/xml$', 'letter_xml', name="letter_xml"),
     url(r'^search/$', 'searchbox', name='searchbox'),
-    url(r'^admin/', include(admin.site.urls))
-#url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+    #url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
    )
 
 # urlpatterns += patterns('',
