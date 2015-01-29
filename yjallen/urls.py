612d8f10da9b3bd81bc2de0707a0e5849d52c24d
Sara Palmer <sepalme@emory.edu>
2015-01-29 16:54:38 -0500

    minor changes

--- yjallen/urls.py
+++ yjallen/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.contrib.sitemaps import views as sitemaps_views
 #from django.contrib.sitemaps import views as sitemaps_views
 #from django.contrib.sitemaps import Sitemap
 
@@ -8,26 +9,26 @@ from django.contrib import admin
 admin.autodiscover()
 
 from yjallen_app.models import LetterTitle
-#from yjallen_app.sitemaps import LetterSitemap
-from yjallen_app.views import index, overview, letter_display, searchbox, letter_xml, send_file
+from yjallen_app.sitemaps import LetterSitemap
+from yjallen_app.views import *
 
 # info_dict = {
 #   'queryset': LetterTitle.objects.all(),
 # }
 
-# sitemaps = {
-#     'letters': LetterSitemap,
-# }
+sitemaps = {
+    'letters': LetterSitemap,
+}
 
-urlpatterns = patterns('yjallen_app.views',
-    url(r'^$', 'index', name='index'),
-    url(r'^overview$', 'overview', name='overview'),
-    url(r'^(?P<doc_id>[^/]+)$', 'letter_display', name="letter_display"),
-    url(r'^(?P<doc_id>[^/]+)/xml$', 'letter_xml', name="letter_xml"),
-    url(r'^search/$', 'searchbox', name='searchbox'),
-    url(r'^(?P<basename>[^/]+)/download$', 'send_file', name='send_file'),
-    #url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-   )
+urlpatterns = patterns('',
+    url(r'^sitemap\.xml$', sitemaps_views.index, {'sitemaps': sitemaps}),
+    url(r'^$', 'yjallen_app.views.index', name='index'),
+    url(r'^overview$', 'yjallen_app.views.overview', name='overview'),
+    url(r'^(?P<doc_id>[^/]+)$', 'yjallen_app.views.letter_display', name="letter_display"),
+    url(r'^(?P<doc_id>[^/]+)/xml$', 'yjallen_app.views.letter_xml', name="letter_xml"),
+    url(r'^search/$', 'yjallen_app.views.searchbox', name='searchbox'),
+    url(r'^(?P<basename>[^/]+)/download$', 'yjallen_app.views.send_file', name='send_file'),
+)
 
 # urlpatterns += patterns('',
 #     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
